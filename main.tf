b652f13b1573685abcf45b4dfa43d83643a74962
Trupti Rewatkar <119307904+Trupti285@users.noreply.github.com>
2025-01-18 20:52:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-cluster"
   cluster_version = "1.30"
-  subnets         = ["subnet-abcde123", "subnet-abcde124"] # Replace with your subnets
-  vpc_id          = "vpc-abcde123" # Replace with your VPC
+  subnets         = ["subnet-041b330fe749e4caa", "subnet-038928c6390cfe743"] 
+  vpc_id          = "vpc-0aa8e9d45b3c7bb42" 
 }
 
 output "cluster_endpoint" {
